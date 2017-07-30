706b5ba2edc31be152f171be9e23cffaf3b5bdc5
Haris <harisasgharmalik@hotmail.com>
2017-07-30 19:45:04 +0200

    Server side json

--- DotNetCoreWebApp/Controllers/HomeController.cs
+++ DotNetCoreWebApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +11,45 @@ namespace DotNetCoreWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<CommentModel> _comments;
+
+        public HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walker",
+                    Text = "This is *another* comment. Multiple comments."
+                },
+            };
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+        [Route("comments")] // attribute routing
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)] // no caching
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
     }
 }
--- DotNetCoreWebApp/wwwroot/js/tutorial.jsx
+++ DotNetCoreWebApp/wwwroot/js/tutorial.jsx
@@ -1,17 +1,10 @@
-﻿var data = [
-    { id: 1, author: "Daniel Lo Nigro", text: "Hello ReactJS.NET World!" },
-    { id: 2, author: "Pete Hunt", text: "This is one comment" },
-    { id: 3, author: "Jordan Walke", text: "This is *another* comment" }
-];
-
-var Comment = React.createClass({
+﻿var Comment = React.createClass({
     rawMarkup: function () {
         var md = new Remarkable();
         var rawMarkup = md.render(this.props.children.toString());
         return { __html: rawMarkup };
     },
     render: function () {
-        var md = new Remarkable();
         return (
             <div className="comment">
                 <h2 className="commentAuthor">
@@ -41,7 +34,6 @@ var CommentList = React.createClass({
     }
 });
 
-
 var CommentForm = React.createClass({
     render: function () {
         return (
@@ -54,19 +46,36 @@ var CommentForm = React.createClass({
 
 
 var CommentBox = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
+    loadCommentsFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+
+    componentDidMount : function() {
+        this.loadCommentsFromServer();
+        window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
     render: function () {
         return (
             <div className="commentBox">
                 Hello, world! I am a CommentBox.
                 <h1>Comments</h1>
-                <CommentList jsonData={this.props.jsonData} />
+                <CommentList jsonData={this.state.data} />
                 <CommentForm />
             </div>
         );
     }
 });
 ReactDOM.render(
-    <CommentBox jsonData={data} />,
+    <CommentBox url="/comments" pollInterval={2000}/>,
     document.getElementById('content')
 );
 
